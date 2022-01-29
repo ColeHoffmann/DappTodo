4ae82e27cf4a3ad96dfc6bda637d8874c3ba5a2c
ColeHoffmann <46608689+ColeHoffmann@users.noreply.github.com>
2022-01-28 23:15:40 -0500

    Created the todo smart contract

--- contracts/todo.sol
+++ contracts/todo.sol
@@ -1,4 +1,15 @@
-pragma solidity ^0.5.0;
-contract Todo { 
-    uint tasks = 0; 
+uint public tasks = 0;
+#Creating the structure with tasks. 
+struct Task {
+    uint id;
+    string content;
+    bool completed;
+    }       
+mapping(uint => Task) public tasks;
+}
+
+#
+function createTask(string memory _content) public {
+    taskCount++;
+    tasks[taskCount] = Task(taskCount, _content, false);
 }
