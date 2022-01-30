d1d7adb1de9d9a41cf817e10df8c66b89d9e727a
ColeHoffmann <46608689+ColeHoffmann@users.noreply.github.com>
2022-01-30 16:33:13 -0500

    todo smart contract fixed and pushed. Nowe need to add in front end.

--- contracts/todo.sol
+++ contracts/todo.sol
@@ -16,9 +16,29 @@ contract Todo {
 
     mapping(uint => Task) public tasks;
 
+
+    event TaskCreated(
+        uint id,    
+        string content,
+        bool completed
+    );
+
     function createTask(string memory _content) public {
         taskCount++;
         tasks[taskCount] = Task(taskCount, _content, false);
     }
+
+    event TaskCompleted(
+        uint id,
+        bool completed
+    );
+
+
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
+    }   
 
 }
