2d81b1d37e847e233304b6abf1606504adaa961f
ColeHoffmann <46608689+ColeHoffmann@users.noreply.github.com>
2022-01-29 21:28:48 -0500

    lol it works

--- contracts/todo.sol
+++ contracts/todo.sol
@@ -1,16 +1,24 @@
 pragma solidity ^0.5.0;
-contract todo{
-uint public tasks = 0;
 
-struct Task {
-    uint id;
-    string content;
-    bool completed;
-    }       
-        mapping(uint => Task) public tasks; 
+contract Todo {
+
+    uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    constructor() public {  
+        createTask("This is our first Task");
+    }
+
+    mapping(uint => Task) public tasks;
 
     function createTask(string memory _content) public {
         taskCount++;
-     tasks[taskCount] = Task(taskCount, _content, false);}
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 
 }
