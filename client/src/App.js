cd4b424b8f540b6e84b3d64063465c3a1e1ea62c
ColeHoffmann <46608689+ColeHoffmann@users.noreply.github.com>
2022-01-31 22:09:13 -0500

    React is now working. Galance got unconnected after running truffle unbox react, so that is fixed as well. Made changes to app.js. The major todo is connecting the react to the smart contracts, then letting that display to the page as well. This will take the chunk of work of the entire project. Also, make things look prettier. We will get there. Coming along :)

--- client/src/App.js
+++ client/src/App.js
@@ -54,17 +54,14 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <h1> Welcome to DappTodo </h1>
+        <h2>a decentralised Todo list that lives on the blockchain. To get started, add an item to the todo list below </h2>
+        <form> 
+          <label> 
+            <p> Type Task below </p>
+            <input task = "task"/>
+            </label>
+        </form>
       </div>
     );
   }
