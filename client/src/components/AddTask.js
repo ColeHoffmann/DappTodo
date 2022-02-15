89a1e867b428e68f826ae90d2ac9e82b5b5df095
ColeHoffmann <46608689+ColeHoffmann@users.noreply.github.com>
2022-02-14 22:04:42 -0500

    having issues with importing smart contracts

--- client/src/components/AddTask.js
+++ client/src/components/AddTask.js
@@ -1,31 +1,16 @@
 import React, { Component } from 'react'
-import Todo from "./contracts/Todo.json";
+
+//FIX THIS
+import TodoContract from "..\\contracts\\Todo.json";
+import Web3 from 'web3'
+
+
 
 class AddTask extends Component { 
 
-    constructor(props) {
-        super(props)
+
     
-        this.state = {
-          TodoInstance: undefined,
-          account: null,
-          web3: null,
-          task:'',
-          id:'',
-          completed = false;
-        }
-      }
 
-
-      updateTask = event => {
-        this.setState({task : event.target.value});
-      }
-
-      updateID = event => {
-        this.setState({task : event.target.id});
-      }
-
-  
 
     render(){
         return(
