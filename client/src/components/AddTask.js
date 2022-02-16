ef2fe28ad80ae900914fcfb4681820b95d2bef35
ColeHoffmann <46608689+ColeHoffmann@users.noreply.github.com>
2022-02-16 00:43:02 -0500

    this is getting hard lol

--- client/src/components/AddTask.js
+++ client/src/components/AddTask.js
@@ -6,6 +6,12 @@ import Web3 from 'web3'
 
 
 
+//throwing this in here for now. Calling these smart contracts has been a huge pain in the ass ngl. 
+//need to check out this documentation and figure this out. 
+//https://web3js.readthedocs.io/en/v1.2.9/
+//const contract = web3.eth.contract(contractAbi); 
+//const contractInstacnce = contract.at(contractAddress)
+
 class AddTask extends Component { 
 
 
